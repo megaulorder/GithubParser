af8e39a4ee19ec05551ba89e444be7c868780435
megaulorder <daydreams7171@gmail.com>
2021-07-04 16:59:05 +1000

    govnokod

--- app/src/main/java/com/example/githubparser/MainActivity.kt
+++ app/src/main/java/com/example/githubparser/MainActivity.kt
@@ -2,40 +2,61 @@ package com.example.githubparser
 
 import android.app.Activity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.widget.Button
 import android.widget.TextView
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
-import org.json.JSONException
+import okhttp3.*
 import org.json.JSONObject
-
+import java.io.IOException
 
 class MainActivity : Activity() {
+    lateinit var textView: TextView
+    lateinit var button: Button
+    lateinit var client: OkHttpClient
+
+    private val REQUEST_URL = "https://api.github.com/search/repositories?q=user:megaulorder&sort=updated&order=desc"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.my_layout)
 
-        val textView = findViewById<TextView>(R.id.text_view_id)
+        textView = findViewById(R.id.text_view_id)
+        button = findViewById(R.id.get_button_id)
 
-        Volley.newRequestQueue(this)
-        val url = "https://api.github.com/search/repositories?q=user:megaulorder&sort=updated&order=desc"
+        textView.movementMethod = ScrollingMovementMethod()
 
-        val stringRequest = StringRequest(url,
-            { response ->
-                try {
-                    val jsonObject = JSONObject(response)
-                    val jsonArray = jsonObject.getJSONArray("items")
-                    for (i in 0 until jsonArray.length()) {
-                        val jo = jsonArray.getJSONObject(i).getString("name")
-                        textView.text =  String.format("%s\n%s", textView.text.toString(), jo)
-                    }
-                } catch (e: JSONException) {
-                    e.printStackTrace()
-                }
-            }) {
-            textView.text = R.string.error_message.toString()
+        button.setOnClickListener {
+            getData()
         }
+    }
 
-        val requestQueue = Volley.newRequestQueue(applicationContext)
-        requestQueue.add(stringRequest)
+    private fun getData() {
+        client = OkHttpClient()
+
+        val request = Request.Builder().url(REQUEST_URL).build()
+
+        client.newCall(request).enqueue(object: Callback {
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+
+                val jsonObject = JSONObject(body)
+                val jsonArray = jsonObject.getJSONArray("items")
+
+                for (i in 0 until jsonArray.length()) {
+                    val jo = jsonArray.getJSONObject(i).getString("name")
+
+                    runOnUiThread {
+                        textView.text = String.format("%s\n%s", textView.text.toString(), jo)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+
+                runOnUiThread {
+                    textView.text = R.string.data_error_message.toString()
+                }
+            }
+        })
     }
 }
